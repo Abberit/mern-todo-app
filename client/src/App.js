be9819573dc4800c73e8dee1d7c1418bfb29f8f7
Alex Klimov <alex.klimov@hotmail.com>
2021-02-03 09:30:12 -0800

    Moved from bootstrap to antd

--- client/src/App.js
+++ client/src/App.js
@@ -1,67 +1,15 @@
+import { Button, version } from "antd";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "antd/dist/antd.css";
 
 function App() {
   return (
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="card card-white">
-            <div class="card-body">
-              <form action="javascript:void(0);">
-                <input type="text" class="form-control add-task" placeholder="New Task..." />
-              </form>
-              <div class="todo-list">
-                <div class="todo-item">
-                  <div class="checker">
-                    <span class="">
-                      <input type="checkbox" />
-                    </span>
-                  </div>
-                  <span>Create theme</span>
-                  <a href="javascript:void(0);" class="float-right remove-todo-item">
-                    <i class="icon-close"></i>
-                  </a>
-                </div>
-                <div class="todo-item">
-                  <div class="checker">
-                    <span class="">
-                      <input type="checkbox" />
-                    </span>
-                  </div>
-                  <span>Work on wordpress</span>
-                  <a href="javascript:void(0);" class="float-right remove-todo-item">
-                    <i class="icon-close"></i>
-                  </a>
-                </div>
+    <div className="App">
+      <h1>antd version: {version}</h1>
 
-                <div class="todo-item">
-                  <div class="checker">
-                    <span class="">
-                      <input type="checkbox" />
-                    </span>
-                  </div>
-                  <span>Organize office main department</span>
-                  <a href="javascript:void(0);" class="float-right remove-todo-item">
-                    <i class="icon-close"></i>
-                  </a>
-                </div>
-                <div class="todo-item">
-                  <div class="checker">
-                    <span>
-                      <input type="checkbox" />
-                    </span>
-                  </div>
-                  <span>Error solve in HTML template</span>
-                  <a href="javascript:void(0);" class="float-right remove-todo-item">
-                    <i class="icon-close"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Button type="primary" style={{ marginLeft: 8 }}>
+        Primary Button
+      </Button>
     </div>
   );
 }
